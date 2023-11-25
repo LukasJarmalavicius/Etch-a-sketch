cec75ab3c26223b046e5adfbc5ee7f89311c4520
LukasJarmalavicius <lukisjarm@gmail.com>
2023-11-25 21:12:45 +0200

    etch a sketch base completed

---
+++ script.js
@@ -0,0 +1,57 @@
+const gridCont = document.querySelector('.gridCont');
+let gridSize = 64;
+let gridWidthHeight = 41;
+const gridCreate = document.querySelector('#createGrid');
+const borderRemove = document.querySelector('#removeBorder');
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+gridCreate.addEventListener('click', () =>{
+    gridCont.innerHTML= ""
+    gridSize = prompt("choose gridsize 1-100")
+    if(gridSize > 100){
+        gridSize = prompt("choose gridsize 1-100");
+    }
+    else{
+    createGrid(gridSize);
+    };
+});
+
+borderRemove.addEventListener('click', () =>{
+    let cellS = document.querySelector('.cell');
+    cellS.style.outline = "0"
+});
+
+function changeColor(e){
+    if (e.type === 'mouseover' && !mouseDown) return
+    else {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+
+function createGrid(gridSize){
+    const grid = document.createElement('div');
+    grid.ondragstart = () => {
+        return false;
+      };
+    grid.className = 'grid'
+    for (let i = 0; i < gridSize; i++){
+        const column = document.createElement('div');
+        column.className = 'column';
+        for (let j = 0; j < gridSize; j++){
+            const cell = document.createElement('div');
+            cell.className = 'cell' ;
+            cell.style.width  = `${gridWidthHeight / gridSize}rem`;
+            cell.style.height = `${gridWidthHeight / gridSize}rem`;
+            cell.textContent = "";
+            column.appendChild(cell);
+            cell.addEventListener('mouseover', changeColor);
+            cell.addEventListener('mousedown', changeColor);
+        }
+        grid.appendChild(column);
+    }
+    gridCont.appendChild(grid);  
+}
